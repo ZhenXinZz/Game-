f93ad07f00c02e3b4fea60627226ac3506ab771c
ZhenXinZz <90728210+ZhenXinZz@users.noreply.github.com>
2022-02-07 22:58:09 +0100

    Add files via upload

---
+++ demarrer.py
@@ -0,0 +1,8 @@
+import pygame
+from.import constant as C
+from.import tools 
+pygame.init()
+SCREEN = pygame.display.set_mode((698,484))
+pygame.display.set_caption('Nian')
+
+GRAPHICS=tools.load_graphics('ressources/imageetsprite')
